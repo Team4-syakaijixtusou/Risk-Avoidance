262e3c95b11f638e175ebc0c39da273396112ea5
{ieuko} <{ieuko1024@gmail.com}>
2024-12-16 13:23:36 +0900

    fix: options

---
+++ src/options/index.js
@@ -0,0 +1,14 @@
+var select = document.getElementById('select');
+
+window.onload = function() {
+    var select = document.getElementById('select');
+    chrome.storage.local.get("chara", function (value){
+        select.options[value.chara].selected = true;
+        console.log(value.chara);
+    });
+}
+
+select.onchange = function(){
+    chrome.storage.local.set({ "chara": select.value});
+}
+
