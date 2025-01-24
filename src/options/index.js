ef0b39db37cca744b50e6afa2f0086f962cd6bb7
KINTA <115142488+KINTAM-Good@users.noreply.github.com>
2025-01-24 09:23:50 +0900

    Add files via upload

--- src/options/index.js
+++ src/options/index.js
@@ -1,17 +1,24 @@
 var select = document.getElementById('select');
 const inputElem = document.getElementById('range');
 var example = document.getElementById('example');
+let apiKey =document.getElementById('apiKey')
 
 window.onload = function() {
     chrome.storage.local.get("chara", function (value){
-        select.options[value.chara].selected = true;
+        //select.options[value.chara].selected = true;
         console.log(value.chara);
     });
     chrome.storage.local.get("fontSize", function (value){
         example.style.fontSize = value.fontSize + "px";
         inputElem.value = value.fontSize;
-    })
+    });
     inputElem.addEventListener('input', rangeOnChange);
+    chrome.storage.local.get("apiKey", function (value){
+        console.log(value.apiKey)
+        if (!(typeof value.apiKey === "undefined")){
+            apiKey.value = value.apiKey;
+        }
+    })
 }
 
 const rangeOnChange = (event) =>{
@@ -25,5 +32,12 @@ const rangeOnChange = (event) =>{
 select.onchange = function(){
     chrome.storage.local.set({ "chara": select.value });
 }
+
+function inputChange(event){
+    chrome.storage.local.set({ "apiKey": event.target.value});
+}
+
+
+apiKey.addEventListener("change", inputChange);
 
 
